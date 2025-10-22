d81bd446583358733a84e22cdba9b9e9f4b7fe7b
cheachento007 <cheachento007@gmail.com>
2025-10-22 14:35:03 +0700

    fix: update api url

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://mini-app-kimchi-git-main-chento007s-projects.vercel.app/api/v1';
+export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://mini-app-kimchi-vmx2.vercel.app/api/v1';
